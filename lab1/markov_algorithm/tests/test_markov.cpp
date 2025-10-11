eeb3c5ade4a8e13f9aa49e8c6089696320444780
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-10-11 09:08:50 +0300

    Update test_markov.cpp

--- lab1/markov_algorithm/tests/test_markov.cpp
+++ lab1/markov_algorithm/tests/test_markov.cpp
@@ -1,14 +1,14 @@
 #include <gtest/gtest.h>
 #include "markov_algorithm.h"
 
-// 1. Простая замена одного символа
+// 1. 
 TEST(MarkovTest, SimpleReplacement) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
     EXPECT_EQ(algo.execute("aaa"), "bbb");
 }
 
-// 2. Замена нескольких символов
+// 2. 
 TEST(MarkovTest, MultipleReplacements) {
     MarkovAlgorithm algo;
     algo.addRule("0", "1");
@@ -16,7 +16,7 @@ TEST(MarkovTest, MultipleReplacements) {
     EXPECT_EQ(algo.execute("101"), "010");
 }
 
-// 3. Финальное правило останавливает выполнение
+// 3. 
 TEST(MarkovTest, FinalRuleStops) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
@@ -24,28 +24,28 @@ TEST(MarkovTest, FinalRuleStops) {
     EXPECT_EQ(algo.execute("aaa"), "c");
 }
 
-// 4. Пустая строка
+// 4.
 TEST(MarkovTest, EmptyInput) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
     EXPECT_EQ(algo.execute(""), "");
 }
 
-// 5. Нет подходящих правил
+// 5. 
 TEST(MarkovTest, NoRulesApply) {
     MarkovAlgorithm algo;
     algo.addRule("x", "y");
     EXPECT_EQ(algo.execute("abc"), "abc");
 }
 
-// 6. Замена пробелов
+// 6. 
 TEST(MarkovTest, SpaceReplacement) {
     MarkovAlgorithm algo;
     algo.addRule(" ", "_");
     EXPECT_EQ(algo.execute("a b c"), "a_b_c");
 }
 
-// 7. Разный регистр
+// 7. 
 TEST(MarkovTest, DifferentCase) {
     MarkovAlgorithm algo;
     algo.addRule("A", "B");
@@ -53,7 +53,7 @@ TEST(MarkovTest, DifferentCase) {
     EXPECT_EQ(algo.execute("Aa"), "Bb");
 }
 
-// 8. Порядок правил имеет значение
+// 8. 
 TEST(MarkovTest, RuleOrder) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
@@ -61,35 +61,35 @@ TEST(MarkovTest, RuleOrder) {
     EXPECT_EQ(algo.execute("a"), "c");
 }
 
-// 9. Одиночная замена
+// 9. 
 TEST(MarkovTest, SingleReplacement) {
     MarkovAlgorithm algo;
     algo.addRule("x", "y");
     EXPECT_EQ(algo.execute("x"), "y");
 }
 
-// 10. Замена слов
+// 10. 
 TEST(MarkovTest, WordReplacement) {
     MarkovAlgorithm algo;
     algo.addRule("cat", "dog");
     EXPECT_EQ(algo.execute("cat"), "dog");
 }
 
-// 11. Спецсимволы
+// 11. 
 TEST(MarkovTest, SpecialChars) {
     MarkovAlgorithm algo;
     algo.addRule("@", "at");
     EXPECT_EQ(algo.execute("user@mail"), "useratmail");
 }
 
-// 12. Числа
+// 12. 
 TEST(MarkovTest, Numbers) {
     MarkovAlgorithm algo;
     algo.addRule("1", "one");
     EXPECT_EQ(algo.execute("1"), "one");
 }
 
-// 13. Очистка правил
+// 13.
 TEST(MarkovTest, ClearRules) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
@@ -97,41 +97,41 @@ TEST(MarkovTest, ClearRules) {
     EXPECT_EQ(algo.execute("a"), "a");
 }
 
-// 14. Финальное правило применяется один раз
+// 14. 
 TEST(MarkovTest, FinalRuleOnce) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b", true);
     EXPECT_EQ(algo.execute("aaa"), "baa");
 }
 
-// 15. Удаление символов
+// 15. 
 TEST(MarkovTest, DeleteChars) {
     MarkovAlgorithm algo;
     algo.addRule("a", "");
     EXPECT_EQ(algo.execute("abc"), "bc");
 }
 
-// 16. Длинные строки
+// 16. 
 TEST(MarkovTest, LongStrings) {
     MarkovAlgorithm algo;
     algo.addRule("t", "T");
     EXPECT_EQ(algo.execute("ttt"), "TTT");
 }
 
-// 17. Правила с пробелами
+// 17. 
 TEST(MarkovTest, RulesWithSpaces) {
     MarkovAlgorithm algo;
     algo.addRule("hello world", "hi");
     EXPECT_EQ(algo.execute("hello world"), "hi");
 }
 
-// 18. Без правил
+// 18.
 TEST(MarkovTest, NoRules) {
     MarkovAlgorithm algo;
     EXPECT_EQ(algo.execute("test"), "test");
 }
 
-// 19. Множественные замены в строке
+// 19. 
 TEST(MarkovTest, MultipleInString) {
     MarkovAlgorithm algo;
     algo.addRule("cat", "dog");
@@ -139,64 +139,64 @@ TEST(MarkovTest, MultipleInString) {
     EXPECT_EQ(algo.execute("red cat"), "blue dog");
 }
 
-// 20. Простой финальный случай
+// 20. 
 TEST(MarkovTest, SimpleFinal) {
     MarkovAlgorithm algo;
     algo.addRule("aa", "b", true);
     EXPECT_EQ(algo.execute("aaa"), "ba");
 }
 
-// 21. Замена на такой же символ
+// 21. 
 TEST(MarkovTest, ReplaceToSame) {
     MarkovAlgorithm algo;
     algo.addRule("a", "a");
     EXPECT_EQ(algo.execute("aaa"), "aaa");
 }
 
-// 22. Несколько одинаковых правил
+// 22. 
 TEST(MarkovTest, DuplicateRules) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
-    algo.addRule("a", "c"); // второе правило никогда не применится
+    algo.addRule("a", "c"); // ГўГІГ®Г°Г®ГҐ ГЇГ°Г ГўГЁГ«Г® Г­ГЁГЄГ®ГЈГ¤Г  Г­ГҐ ГЇГ°ГЁГ¬ГҐГ­ГЁГІГ±Гї
     EXPECT_EQ(algo.execute("a"), "b");
 }
 
-// 23. Замена подстроки
+// 23. 
 TEST(MarkovTest, SubstringReplacement) {
     MarkovAlgorithm algo;
     algo.addRule("ab", "ba");
     EXPECT_EQ(algo.execute("abc"), "bac");
 }
 
-// 24. Пустая замена в середине
+// 24. 
 TEST(MarkovTest, EmptyReplacementMiddle) {
     MarkovAlgorithm algo;
     algo.addRule("b", "");
     EXPECT_EQ(algo.execute("abc"), "ac");
 }
 
-// 25. Замена в начале строки
+// 25. 
 TEST(MarkovTest, ReplaceAtStart) {
     MarkovAlgorithm algo;
     algo.addRule("a", "x");
     EXPECT_EQ(algo.execute("abc"), "xbc");
 }
 
-// 26. Замена в конце строки
+// 26. 
 TEST(MarkovTest, ReplaceAtEnd) {
     MarkovAlgorithm algo;
     algo.addRule("c", "x");
     EXPECT_EQ(algo.execute("abc"), "abx");
 }
 
-// 27. Многобуквенные правила
+// 27.
 TEST(MarkovTest, MultiCharRules) {
     MarkovAlgorithm algo;
     algo.addRule("abc", "xyz");
     EXPECT_EQ(algo.execute("abc"), "xyz");
 }
 
-// 28. Цепочка замен
+// 28. 
 TEST(MarkovTest, ChainReplacement) {
     MarkovAlgorithm algo;
     algo.addRule("a", "b");
@@ -205,7 +205,7 @@ TEST(MarkovTest, ChainReplacement) {
     EXPECT_EQ(algo.execute("a"), "d");
 }
 
-// 29. Смешанные символы
+// 29. 
 TEST(MarkovTest, MixedChars) {
     MarkovAlgorithm algo;
     algo.addRule("1", "one");
@@ -213,7 +213,7 @@ TEST(MarkovTest, MixedChars) {
     EXPECT_EQ(algo.execute("1a"), "oneA");
 }
 
-// 30. Простой тест на покрытие
+// 30.
 TEST(MarkovTest, CoverageTest) {
     MarkovAlgorithm algo;
     algo.addRule("test", "passed");
@@ -223,4 +223,5 @@ TEST(MarkovTest, CoverageTest) {
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+
 }

f8489dfe3e4c4897e29650226484905d5ace4b62
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-11-07 13:20:20 +0300

    Update Route.cpp

--- lab2/Logistics/Route.cpp
+++ lab2/Logistics/Route.cpp
@@ -1,5 +1,9 @@
 #include "Route.h"
 
+namespace {
+    const double DEFAULT_AVERAGE_SPEED_KMH = 60.0;
+}
+
 Route::Route(const std::string& id, Address* start, Address* end)
     : routeId(id), startPoint(start), endPoint(end), totalDistance(0), estimatedTime(0) {
 }
@@ -17,13 +21,15 @@ void Route::addWaypoint(GPSPosition* point) {
 }
 
 void Route::calculateRoute() {
-    // Простой расчет расстояния
     if (startPoint->getGPSPosition() && endPoint->getGPSPosition()) {
         totalDistance = startPoint->getGPSPosition()->calculateDistance(*endPoint->getGPSPosition());
-        estimatedTime = totalDistance / 60.0; // часов при скорости 60 км/ч
+        estimatedTime = totalDistance / DEFAULT_AVERAGE_SPEED_KMH;
     }
 }
 
 double Route::calculateETA(double speed) const {
+    if (speed <= 0) {
+        speed = DEFAULT_AVERAGE_SPEED_KMH;
+    }
     return totalDistance / speed;
 }

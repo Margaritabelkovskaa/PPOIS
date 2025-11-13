d315b505633627353f60de203f33716aab9bd09d
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-11-13 21:22:54 +0300

    Update Magazine.h

--- lab3/LibtaryItems/Magazine.h
+++ lab3/LibtaryItems/Magazine.h
@@ -2,8 +2,8 @@
 #define MAGAZINE_H
 
 #include "LibraryItem.h"
-
-using namespace std;
+#include "Publisher.h"   
+#include "Article.h"  
 
 class Magazine : public LibraryItem {
 private:
@@ -11,21 +11,25 @@ private:
     int issueNumber;
     string publicationDate;
     string frequency;
+    shared_ptr<Publisher> publisher;  
+    vector<shared_ptr<Article>> articles;  
 
 public:
-    Magazine(const string& issn, const string& title, int issueNumber,
-        const string& publicationDate, const string& frequency = "monthly");
+    Magazine(const string& issn, const string& title, 
+             const shared_ptr<Publisher>& publisher, int issueNumber,
+             const string& publicationDate, const string& frequency = "monthly");
 
+    // Существующие методы + новые
     bool validateISSN() const;
     bool isCurrentIssue() const;
     double calculateReplacementCost() const override;
     bool isQuarterly() const;
     int getIssueYear() const;
-
-    string getISSN() const;
-    int getIssueNumber() const;
-    string getPublicationDate() const;
-    string getFrequency() const;
+    
+    void addArticle(const shared_ptr<Article>& article);
+    vector<shared_ptr<Article>> getArticles() const;
+    shared_ptr<Publisher> getPublisher() const;  // новый геттер
 };
+
 
 #endif

074dee5ab184c3a9029a7ec14e7dfc2366aabfad
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-11-20 18:34:36 +0300

    Add files via upload

--- lab3/Employees/ArchiveWorker.cpp
+++ lab3/Employees/ArchiveWorker.cpp
@@ -1,15 +1,29 @@
 
 #include "ArchiveWorker.h"
 
-ArchiveWorker::ArchiveWorker(int id, const string & name, const string & position,
-    double salary, const shared_ptr<Department>&department)
-    : Employee(id, name, position, salary, department) {
+ArchiveWorker::ArchiveWorker(int id, const std::string& name, const std::string& position,
+    double salary, const std::shared_ptr<Department>& department,
+    bool hasAccessToRareMaterials)
+    : Employee(id, name, position, salary, department),
+    hasAccessToRareMaterials(hasAccessToRareMaterials) {
 }
 
-bool ArchiveWorker::canAccessArchive() const {
+bool ArchiveWorker::canProcessLoans() const {
+    return false; // Работники архива не могут обрабатывать займы
+}
+
+std::string ArchiveWorker::getRole() const {
+    return "Archive Worker";
+}
+
+bool ArchiveWorker::canAccessRareMaterials() const {
+    return hasAccessToRareMaterials;
+}
+
+bool ArchiveWorker::canRestoreDocuments() const {
     return true;
 }
 
-string ArchiveWorker::getArchiveWorkerInfo() const {
-    return "Archive Worker: " + getName();
+void ArchiveWorker::grantRareMaterialsAccess(bool access) {
+    hasAccessToRareMaterials = access;
 }
--- lab3/Employees/ArchiveWorker.h
+++ lab3/Employees/ArchiveWorker.h
@@ -1,16 +1,25 @@
+
 #ifndef ARCHIVEWORKER_H
 #define ARCHIVEWORKER_H
 
 #include "Employee.h"
 
 class ArchiveWorker : public Employee {
+private:
+    bool hasAccessToRareMaterials;
+
 public:
-    ArchiveWorker(int id, const string& name, const string& position,
-        double salary, const shared_ptr<Department>& department);
+    ArchiveWorker(int id, const std::string& name, const std::string& position,
+        double salary, const std::shared_ptr<Department>& department,
+        bool hasAccessToRareMaterials = false);
 
-    bool canAccessArchive() const;
-    string getArchiveWorkerInfo() const;
+    bool canProcessLoans() const override;
+    std::string getRole() const override;
+
+    bool canAccessRareMaterials() const;
+    bool canRestoreDocuments() const;
+    void grantRareMaterialsAccess(bool access);
 };
-#endif
 
+#endif
 

f40cad2b91d9f42ca35087634d68e518b5fb543b
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-11-20 16:04:51 +0300

    Update FineCalculator.h

--- lab3/Loan/FineCalculator.h
+++ lab3/Loan/FineCalculator.h
@@ -1,20 +1,24 @@
 #ifndef FINECALCULATOR_H
 #define FINECALCULATOR_H
 
-#include "Book.h"
-#include "EBook.h"
-#include "Audiobook.h"
-#include "Magazine.h"
-#include "StudentBook.h"
-#include "Article.h"
+#include "LibraryItem.h"
 #include <memory>
-
-using namespace std;
 
 class FineCalculator {
 public:
-    static double calculateLateFine(const shared_ptr<LibraryItem>& item, int daysLate);
-    static double calculateDamageFine(const shared_ptr<LibraryItem>& item, const string& damageType);
+    // Процент от стоимости за день просрочки
+    static const double DAILY_LATE_PERCENTAGE;
+    
+    // Проценты от стоимости за разные типы повреждений
+    static const double MINOR_DAMAGE_PERCENTAGE;
+    static const double MODERATE_DAMAGE_PERCENTAGE; 
+    static const double SEVERE_DAMAGE_PERCENTAGE;
+    static const double LOST_ITEM_PERCENTAGE;
+
+    // Расчет штрафов
+    static double calculateLateFine(const std::shared_ptr<LibraryItem>& item, int daysLate);
+    static double calculateDamageFine(const std::shared_ptr<LibraryItem>& item, const std::string& damageType);
+    static double calculateLostItemFine(const std::shared_ptr<LibraryItem>& item);
 };
 
 #endif

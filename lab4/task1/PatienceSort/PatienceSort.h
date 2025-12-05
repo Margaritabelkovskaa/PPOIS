95fbf2b625530ae2e71abaa9e8422d6f5efb86b3
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-12-05 21:56:13 +0300

    Update PatienceSort.h

--- lab4/task1/PatienceSort/PatienceSort.h
+++ lab4/task1/PatienceSort/PatienceSort.h
@@ -4,47 +4,49 @@
 #include <iterator>
 
 template <typename T>
-void patienceSort(std::vector<T>& data) {
-    if (data.empty()) return;
+class PatienceSort {
+public:
+    static void sort(std::vector<T>& data) {
+        if (data.empty()) return;
 
-    std::vector<std::vector<T>> piles;
+        std::vector<std::vector<T>> piles;
 
-    for (const T& element : data) {
-        bool placed = false;
-        for (auto& pile : piles) {
-            if (pile.back() >= element) {
-                pile.push_back(element);
-                placed = true;
-                break;
+        for (const T& element : data) {
+            bool placed = false;
+            for (auto& pile : piles) {
+                if (pile.back() >= element) {
+                    pile.push_back(element);
+                    placed = true;
+                    break;
+                }
             }
-        }
-        if (!placed) {
-            piles.push_back(std::vector<T>{element});
-        }
-    }
-
-    data.clear();
-
-    while (!piles.empty()) {
-        size_t minIndex = 0;
-        for (size_t i = 1; i < piles.size(); ++i) {
-            if (piles[i].back() < piles[minIndex].back()) {
-                minIndex = i;
+            if (!placed) {
+                piles.push_back(std::vector<T>{element});
             }
         }
 
-        data.push_back(piles[minIndex].back());
-        piles[minIndex].pop_back();
+        data.clear();
 
-        if (piles[minIndex].empty()) {
-            piles.erase(piles.begin() + minIndex);
+        while (!piles.empty()) {
+            size_t minIndex = 0;
+            for (size_t i = 1; i < piles.size(); ++i) {
+                if (piles[i].back() < piles[minIndex].back()) {
+                    minIndex = i;
+                }
+            }
+
+            data.push_back(piles[minIndex].back());
+            piles[minIndex].pop_back();
+
+            if (piles[minIndex].empty()) {
+                piles.erase(piles.begin() + minIndex);
+            }
         }
     }
-}
 
-template <typename T>
-void patienceSort(T* array, size_t size) {
-    std::vector<T> temp(array, array + size);
-    patienceSort(temp);
-    std::copy(temp.begin(), temp.end(), array);
-}
+    static void sort(T* array, size_t size) {
+        std::vector<T> temp(array, array + size);
+        sort(temp);
+        std::copy(temp.begin(), temp.end(), array);
+    }
+};

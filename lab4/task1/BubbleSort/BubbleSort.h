6eb5b2e37c2b8222682a643a1b672db62f840bf5
Margaritabelkovskaa <margaritabelkovskaa@gmail.com>
2025-12-05 21:47:23 +0300

    Update BubbleSort.h

--- lab4/task1/BubbleSort/BubbleSort.h
+++ lab4/task1/BubbleSort/BubbleSort.h
@@ -3,34 +3,36 @@
 #include <algorithm>
 
 template <typename T>
-void bubbleSort(std::vector<T>& data) {
-    if (data.empty()) return;
+class BubbleSort {
+public:
+    static void sort(std::vector<T>& data) {
+        if (data.empty()) return;
 
-    size_t n = data.size();
-    for (size_t i = 0; i < n - 1; ++i) {
-        bool swapped = false;
-        for (size_t j = 0; j < n - i - 1; ++j) {
-            if (data[j] > data[j + 1]) {
-                std::swap(data[j], data[j + 1]);
-                swapped = true;
+        size_t n = data.size();
+        for (size_t i = 0; i < n - 1; ++i) {
+            bool swapped = false;
+            for (size_t j = 0; j < n - i - 1; ++j) {
+                if (data[j] > data[j + 1]) {
+                    std::swap(data[j], data[j + 1]);
+                    swapped = true;
+                }
             }
+            if (!swapped) break;
         }
-        if (!swapped) break;
     }
-}
 
-template <typename T>
-void bubbleSort(T* array, size_t size) {
-    if (size == 0) return;
+    static void sort(T* array, size_t size) {
+        if (size == 0) return;
 
-    for (size_t i = 0; i < size - 1; ++i) {
-        bool swapped = false;
-        for (size_t j = 0; j < size - i - 1; ++j) {
-            if (array[j] > array[j + 1]) {
-                std::swap(array[j], array[j + 1]);
-                swapped = true;
+        for (size_t i = 0; i < size - 1; ++i) {
+            bool swapped = false;
+            for (size_t j = 0; j < size - i - 1; ++j) {
+                if (array[j] > array[j + 1]) {
+                    std::swap(array[j], array[j + 1]);
+                    swapped = true;
+                }
             }
+            if (!swapped) break;
         }
-        if (!swapped) break;
     }
-}
+};
